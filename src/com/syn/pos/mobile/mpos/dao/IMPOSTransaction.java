8b7759531d1987241e20c72f7470fc971856181a
Jitthapong-Synature <jitthapong@synaturegroup.com>
2013-07-23 18:45:41 +0700

    added MPOSTransaction class

--- src/com/syn/pos/mobile/mpos/dao/IMPOSTransaction.java
+++ src/com/syn/pos/mobile/mpos/dao/IMPOSTransaction.java
@@ -2,9 +2,13 @@ package com.syn.pos.mobile.mpos.dao;
 
 public interface IMPOSTransaction {
 	public int getMaxTransaction(int shopId, int computerId);
-	public void openTransaction(int transactionId, int computerId, 
-			int shopId, int sessionId, int staffId);
-	public void updateTransaction(int transactionId, int computerId, int staffId, 
-			double transVat, double transExclVat, double serviceCharge);
+
+	public void openTransaction(int transactionId, int computerId, int shopId,
+			int sessionId, int staffId);
+
+	public void updateTransaction(int transactionId, int computerId,
+			int staffId, double transVat, double transExclVat,
+			double serviceCharge);
+
 	public void closeTransaction();
 }
