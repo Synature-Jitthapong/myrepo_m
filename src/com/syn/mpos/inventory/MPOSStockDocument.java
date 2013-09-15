15f79d2435c6be21da176220ac4fc89fbcb4ef04
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-09-15 21:19:52 +0700

    update

---
+++ src/com/syn/mpos/inventory/MPOSStockDocument.java
@@ -0,0 +1,13 @@
+package com.syn.mpos.inventory;
+
+import com.syn.pos.inventory.StockDocument;
+
+public class MPOSStockDocument extends StockDocument {
+
+	@Override
+	public boolean createDocument(int shopId, int documentTypeId, int staffId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
