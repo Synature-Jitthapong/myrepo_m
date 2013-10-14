eb38cb314b592035a4160728b0f69794f263b5e5
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-10-14 18:05:42 +0700

    update

--- src/com/syn/mpos/SettingActivity.java
+++ src/com/syn/mpos/SettingActivity.java
@@ -2,6 +2,8 @@ package com.syn.mpos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.ListFragment;
 import android.view.Menu;
 
 public class SettingActivity extends Activity {
@@ -14,9 +16,15 @@ public class SettingActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.setting, menu);
 		return true;
 	}
 
+	public static class SettingCategoryFragment extends ListFragment{
+		
+	}
+	
+	public static class SettingFragment extends Fragment{
+		
+	}
 }
