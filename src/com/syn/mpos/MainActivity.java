7e3dc5ee627774c4fc5ff4eab3e6a8b812406f77
Synature-Jitthapong <jitthapong@synaturegroup.com>
2014-04-16 10:12:04 +0700

    update

--- src/com/syn/mpos/MainActivity.java
+++ src/com/syn/mpos/MainActivity.java
@@ -51,10 +51,12 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -62,7 +64,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
-public class MainActivity extends FragmentActivity implements MenuPageFragment.OnMenuItemClick,
+public class MainActivity extends FragmentActivity implements
 	OnItemClickListener, OnClickListener, OnKeyListener{
 	
 	public static final int TAB_UNDERLINE_COLOR = 0xFF1D78B2;
@@ -582,6 +584,77 @@ public class MainActivity extends FragmentActivity implements MenuPageFragment.O
 		}
 	}
 	
+	public static class MenuPageFragment extends Fragment {
+		
+		private List<Products.Product> mProductLst;
+		private MenuItemAdapter mAdapter;
+		private int mDeptId;
+		
+		public static MenuPageFragment newInstance(int deptId){
+			MenuPageFragment f = new MenuPageFragment();
+			Bundle b = new Bundle();
+			b.putInt("deptId", deptId);
+			f.setArguments(b);
+			return f;
+		}
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			if((savedInstanceState != null) && savedInstanceState.containsKey("deptId")){
+				mDeptId = savedInstanceState.getInt("deptId");
+			}
+			else{
+				mDeptId = getArguments().getInt("deptId");
+			}
+		}
+		
+		@Override
+		public void onSaveInstanceState(Bundle outState) {
+			super.onSaveInstanceState(outState);
+			outState.putInt("deptId", mDeptId);
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+
+			final GridView gvItem = (GridView) inflater.inflate(R.layout.menu_grid_view, container, false);
+			mProductLst = ((MainActivity) getActivity()).getProduct().listProduct(mDeptId);
+			mAdapter = new MenuItemAdapter(getActivity(), 
+					((MainActivity) getActivity()).getDatabase(), mProductLst);
+			gvItem.setAdapter(mAdapter);
+			gvItem.setOnItemClickListener(new OnItemClickListener(){
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View v, int position,
+						long id) {
+					Products.Product p = 
+							(Products.Product) parent.getItemAtPosition(position);
+					
+					((MainActivity) getActivity()).onMenuClick(p.getProductId(), p.getProductTypeId(), 
+							p.getVatType(), p.getVatRate(), p.getProductPrice());
+				}
+			});
+			
+			gvItem.setOnItemLongClickListener(new OnItemLongClickListener(){
+				
+				@Override
+				public boolean onItemLongClick(AdapterView<?> parent, View v,
+						int position, long id) {
+					Products.Product p = (Products.Product) parent.getItemAtPosition(position);
+					ImageViewPinchZoom imgZoom = ImageViewPinchZoom.newInstance(p.getImgUrl(), p.getProductName(), 
+							GlobalProperty.currencyFormat(
+									((MainActivity) getActivity()).getDatabase(), p.getProductPrice()));
+					imgZoom.show(getFragmentManager(), "MenuImage");
+					return true;
+				}
+				
+			});
+			return gvItem;
+		}
+	}
+		
 	public void clearBillClicked(final View v){
 		new AlertDialog.Builder(MainActivity.this)
 		.setIcon(android.R.drawable.ic_dialog_alert)
@@ -950,7 +1023,6 @@ public class MainActivity extends FragmentActivity implements MenuPageFragment.O
 		}
 	}
 	
-	@Override
 	public void onMenuClick(int productId, int productTypeId, int vatType, double vatRate, double productPrice) {
 		if(productTypeId == Products.NORMAL_TYPE || 
 				productTypeId == Products.SET_TYPE){
