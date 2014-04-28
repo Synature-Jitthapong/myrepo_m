d22e92516e60b07d35a803466d6899a8ebc77376
Synature-Jitthapong <jitthapong@synaturegroup.com>
2014-04-28 14:15:04 +0700

    update

--- src/com/syn/mpos/database/MPOSManager.java
+++ src/com/syn/mpos/database/MPOSManager.java
@@ -1,15 +1,46 @@
 package com.syn.mpos.database;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public class MPOSManager {
 	
-	private SQLiteDatabase mSqlite;
+	private MPOSSQLiteHelper mSqliteHelper;
+	
+	private OrderTransactionDataSource mTransaction;
 	
 	private int mShopId;
 	private int mStaffId;
 	private int mTransactionId;
 	private int mComputerId;
 	
+	/*
+	 * total price
+	 */
+	private double mTotalPrice;
+	
+	/*
+	 * total discount
+	 */
+	private double mTotalDiscount;
+	
+	/*
+	 * total sale price
+	 */
 	private double mTotalSalePrice;
+	
+	/*
+	 * total vat
+	 */
+	private double mTotalVat;
+	
+	public MPOSManager(Context c, int staffId){
+		mSqliteHelper = new MPOSSQLiteHelper(c);
+		
+		
+	}
+	
+	private SQLiteDatabase open(){
+		return mSqliteHelper.getWritableDatabase();
+	}
 }
